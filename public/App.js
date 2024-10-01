acb15cc1d4b47340bbae8217d9e25817fee510ed
Jadeung123 <jedsada6710@gmail.com>
2024-10-01 20:27:24 +0700

    Fix MongoDB connection issue7

--- public/App.js
+++ public/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('https://mineversus-a27cf5ba45b1.herokuapp.com');
+const socket = io('wss://mineversus-a27cf5ba45b1.herokuapp.com');
 
 const App = () => {
   const [gameState, setGameState] = useState(null);
